a20efd23ebc9c18ccc48f780e393a4b2fc7fa80a
Matthew Pandolfo <matt@pandolfo.com>
2021-09-15 14:26:01 -0400

    validates if questions are filled in

--- app.js
+++ app.js
@@ -13,12 +13,28 @@ const promptProject = (portfolioData) => {
       {
         type: "input",
         name: "name",
-        message: "What is the name of your project?",
+        message: "What is the name of your project? (Required)",
+        validate: (nameInput) => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log("Please enter the name of your project");
+            return false;
+          }
+        },
       },
       {
         type: "input",
         name: "description",
         message: "Provide a description of the project. (Required)",
+        validate: (nameInput) => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log("Please enter a description");
+            return false;
+          }
+        },
       },
       {
         type: "checkbox",
@@ -38,6 +54,14 @@ const promptProject = (portfolioData) => {
         type: "input",
         name: "link",
         message: "Enter the GitHub link to your project. (Required)",
+        validate: (nameInput) => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log("Please enter the link to your project");
+            return false;
+          }
+        },
       },
       {
         type: "confirm",
@@ -67,12 +91,28 @@ const promptUser = () => {
     {
       type: "input",
       name: "name",
-      message: "What is your name?",
+      message: "What is your name? (Required)",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter your name");
+          return false;
+        }
+      },
     },
     {
       type: "input",
       name: "github",
-      message: "What is your GitHub Username?",
+      message: "What is your GitHub Username? (Required)",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter your GitHub Username");
+          return false;
+        }
+      },
     },
     {
       type: "input",
